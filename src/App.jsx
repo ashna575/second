e58b1d0e91d289cf704329d2d9351fdfd50c48c1
ashna23 <ashnaraikwar9@gmail.com>
2025-06-20 14:31:44 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  
+
+return (
+  <div className="text-pink-200 text-4xl font-serif italic p-6 ">
+  hello 
+  </div>
+);
+}
+export default App
